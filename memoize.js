053237d1296a8aeef3e4c0d7642736f77b8bf13b
addyosmani <addyosmani@gmail.com>
2011-09-21 10:35:04 +0100

    adding the improvements from dmitry

--- memoize.js
+++ memoize.js
@@ -1,24 +1,22 @@
 /*
 * memoize.js
 * by @philogb and @addyosmani
-* further optimizations by @mathias
+* with further optimizations by @mathias
+* and @DmitryBaranovsk
+* perf tests: http://bit.ly/q3zpG3
 * Released under an MIT license.
 */
-function memoize( fn ) {  
-    return function () {  
-        var args = Array.prototype.slice.call(arguments),  
-            hash = "",  
-            i  = args.length;  
-            toString = Object.prototype.toString,
-            callEm = toString.call({}),
-            currentArg = null;
-        while(i--){  
-          currentArg = args[i];
-          hash += (callEm == toString.call(currentArg)) ?   
-          JSON.stringify(currentArg) : currentArg;  
-          fn.memoize || (fn.memoize = {});  
-      }   
-        return (hash in fn.memoize) ? fn.memoize[hash] :   
-        fn.memoize[hash] = fn.apply( this , args );  
-    };  
-}  
+function memoize( fn ) {
+    return function () {
+        var args = Array.prototype.slice.call(arguments),
+            hash = "",
+            i = args.length;
+        currentArg = null;
+        while (i--) {
+            currentArg = args[i];
+            hash += (currentArg === Object(currentArg)) ? JSON.stringify(currentArg) : currentArg;
+            fn.memoize || (fn.memoize = {});
+        }
+        return (hash in fn.memoize) ? fn.memoize[hash] : fn.memoize[hash] = fn.apply(this, args);
+    };
+}
