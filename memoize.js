12d95713f4c5483123f99124b0632b39df2f2dd3
addyosmani <addyosmani@gmail.com>
2011-09-21 14:45:18 +0100

    adding use strict, minor formatting updates as per lint test

--- memoize.js
+++ memoize.js
@@ -6,12 +6,13 @@
 * perf tests: http://bit.ly/q3zpG3
 * Released under an MIT license.
 */
-function memoize( fn ) {
+function memoize(fn) {
+  "use strict";
     return function () {
         var args = Array.prototype.slice.call(arguments),
             hash = "",
-            i = args.length;
-        currentArg = null;
+            i = args.length,
+            currentArg = null;
         while (i--) {
             currentArg = args[i];
             hash += (currentArg === Object(currentArg)) ? JSON.stringify(currentArg) : currentArg;
