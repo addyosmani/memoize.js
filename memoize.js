b31d697f5c02784e29d99c0a155f9249a374b68c
才志佳人 <jiahut@gmail.com>
2013-04-11 13:43:53 +0800

    Update memoize.js
    it can get and remove the cache result within the cachedfuc

--- memoize.js
+++ memoize.js
@@ -13,10 +13,18 @@
             var stringifyJson = JSON.stringify,
                 cache = {};
 
-            return function () {
+            var cachedfun = function () {
                 var hash = stringifyJson(arguments);
                 return (hash in cache) ? cache[hash] : cache[hash] = func.apply(this, arguments);
             };
+            cachedfun.__cache = (function(){
+                cache.remove || (cache.remove = function(){
+                    var hash = stringifyJson(arguments);
+                    return (delete cache[hash]);
+                });
+                return cache;
+            }).call(this);
+            return cachedfun;
         } : function (func) {
             return func;
         }));
